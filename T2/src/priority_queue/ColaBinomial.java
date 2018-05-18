7f65f00111cf6a9c05bdb453279b98be028afb40
Daniton2906 <ddiomedi2906@gmail.com>
2018-05-17 22:06:56 -0400

    Start implementing ColaBinomial Create Binomial_Tree inner class for ColaBinomial

--- T2/src/priority_queue/ColaBinomial.java
+++ T2/src/priority_queue/ColaBinomial.java
@@ -2,9 +2,52 @@ package priority_queue;
 
 import utils.Node;
 
-public class ColaBinomial implements PriorityQueue{
+import java.util.LinkedList;
 
-    public void insertar(int x, int p){}
+public class ColaBinomial implements PriorityQueue {
+
+    private class Binomial_Tree {
+
+        private LinkedList<Binomial_Tree> binomial_trees;
+        private int order;
+        private Node node;
+
+        private Binomial_Tree(Node node) {
+            this.binomial_trees = new LinkedList<>();
+            this.order = 0;
+            this.node = node;
+        }
+
+        private Binomial_Tree(Binomial_Tree t0, Binomial_Tree t1) {
+            assert t0.order == t1.order;
+            this.binomial_trees = new LinkedList<>();
+            if(t0.node.getPriority() >= t1.node.getPriority()) {
+                this.binomial_trees.addAll(t0.binomial_trees);
+                this.binomial_trees.addLast(t1);
+                this.node = t0.node;
+                this.order = t0.order + 1;
+            } else {
+                this.binomial_trees.addAll(t1.binomial_trees);
+                this.binomial_trees.addLast(t0);
+                this.node = t1.node;
+                this.order = t1.order + 1;
+            }
+        }
+    }
+
+    private LinkedList<Binomial_Tree> binomial_forest;
+
+    public ColaBinomial() {
+        this.binomial_forest = new LinkedList<>();
+    }
+
+    // suma
+    public ColaBinomial(ColaBinomial c0, ColaBinomial c1) {
+        int total_nodes = (int) (Math.pow(2, c0.count_nodes()) + Math.pow(2, c1.count_nodes()));
+
+    }
+
+    public void insertar(int x, int p) {}
 
     public Node extraer_siguiente(){return null;}
 
@@ -12,5 +55,12 @@ public class ColaBinomial implements PriorityQueue{
         return true;
     }
 
-    public PriorityQueue meld(PriorityQueue c0, PriorityQueue c1){return null;}
+    private int count_nodes(){
+        int res = 0;
+        for (Binomial_Tree bt: binomial_forest) {
+            res += (int) Math.pow(2, bt.order);
+        }
+        return res;
+    }
+
 }
