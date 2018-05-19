57ca6f81018c6280e38e2049f475038804c533d0
Daniton2906 <ddiomedi2906@gmail.com>
2018-05-19 00:00:40 -0400

    Implement Sort application

--- T2/src/apps/Sort.java
+++ T2/src/apps/Sort.java
@@ -1,6 +1,34 @@
 package apps;
 
+import priority_queue.HeapClasico;
+import priority_queue.PriorityQueue;
+import utils.Node;
+
+import java.util.List;
+
 public class Sort {
 
+    private List<Node> nodes;
+    private PriorityQueue cp;
+
+    public Sort(List<Node> nodes, PriorityQueue cp) {
+        this.nodes = nodes;
+        this.cp = cp;
+        for(Node node : nodes)
+            cp.insertar(node.getValue(), node.getPriority());
+
+    }
+
+    public Sort(List<Node> nodes) {
+        this.nodes = nodes;
+        this.cp = new HeapClasico(nodes);
+    }
+
+    public void sort() {
+        int k = 0;
+        while (!cp.isEmpty()) {
+            nodes.set(k, cp.extraer_siguiente());
+        }
+    }
 
 }
