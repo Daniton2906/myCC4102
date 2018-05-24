81a34f96b7a39a6678cb8d096485abd2d7f9ce0b
Rukua95 <tswysen95@gmail.com>
2018-05-23 23:34:45 -0400

    Adding to test leftist heap and skew heap.

--- T2/src/main/Main.java
+++ T2/src/main/Main.java
@@ -1,9 +1,6 @@
 package main;
 
-import priority_queue.ColaBinomial;
-import priority_queue.ColaFibonacci;
-import priority_queue.HeapClasico;
-import priority_queue.PriorityQueue;
+import priority_queue.*;
 import utils.DataManager;
 import utils.Tester;
 
@@ -19,9 +16,14 @@ public class Main {
         DataManager dm = new DataManager(N, 0);
         PriorityQueue heapClasico = new HeapClasico(),
                 colaBinomial = new ColaBinomial(),
-                colaFibonacci = new ColaFibonacci();
+                colaFibonacci = new ColaFibonacci(),
+                leftistheap = new LeftistHeap(),
+                skewheap = new SkewHeap();
+        
         //Tester.test0(heapClasico, dm, false);
         //Tester.test0(colaBinomial, dm, false);
         Tester.test0(colaFibonacci, dm, false);
+        //Tester.test0(letistheap, dm, false);
+        //Tester.test0(skewheap, dm, false);
     }
 }
