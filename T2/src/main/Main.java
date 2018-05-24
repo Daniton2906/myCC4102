a040107f3614b9d1b9beba0fa948b8282781887e
Daniton2906 <ddiomedi2906@gmail.com>
2018-05-24 17:43:29 -0400

    Add sort and insert_and_melding tests

--- T2/src/main/Main.java
+++ T2/src/main/Main.java
@@ -1,6 +1,10 @@
 package main;
 
-import priority_queue.*;
+import org.junit.Test;
+import priority_queue.ColaBinomial;
+import priority_queue.ColaFibonacci;
+import priority_queue.HeapClasico;
+import priority_queue.PriorityQueue;
 import utils.DataManager;
 import utils.Tester;
 
@@ -11,28 +15,22 @@ public class Main {
     private static final int N = 10;
 
     public static void main (String [ ] args) throws IOException {
-        System.out.println("Ejecucion Main");
+        System.out.println("Hello World");
 
         DataManager dm = new DataManager(N, 0);
         PriorityQueue heapClasico = new HeapClasico(),
                 colaBinomial = new ColaBinomial(),
-                colaFibonacci = new ColaFibonacci(),
-                leftistheap = new LeftistHeap(),
-                skewheap = new SkewHeap();
+                colaFibonacci = new ColaFibonacci();
+        //Tester.test0(heapClasico, dm, false);
+        //Tester.test0(colaBinomial, dm, false);
+        //Tester.test0(colaFibonacci, dm, false);
 
-        System.out.println("  Test heap clasico");
-        Tester.test0(heapClasico, dm, false);
+        //Tester.sort_test(heapClasico, false, "heap-clasico");
+        //Tester.sort_test(colaBinomial, false, "cola-binomial");
+        //Tester.sort_test(colaFibonacci, false, "cola-fibonacci");
 
-        System.out.println("  Test cola binomial");
-        Tester.test0(colaBinomial, dm, false);
-
-        System.out.println("  Test cola fibonacci");
-        Tester.test0(colaFibonacci, dm, false);
-
-        System.out.println("  Test leftist heap");
-        Tester.test0(leftistheap, dm, false);
-
-        System.out.println("  Test skew heap");
-        Tester.test0(skewheap, dm, false);
+        Tester.insert_and_melding_test(heapClasico, false, "heap-clasico");
+        Tester.insert_and_melding_test(colaBinomial, false, "cola-binomial");
+        Tester.insert_and_melding_test(colaFibonacci, false, "cola-fibonacci");
     }
 }
--- T2/src/utils/Tester.java
+++ T2/src/utils/Tester.java
@@ -1,38 +1,134 @@
 package utils;
 
+import apps.InsertAndMelding;
+import apps.Sort;
 import priority_queue.PriorityQueue;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Vector;
 
 public class Tester {
 
+    private static final int MIN_EXP_SORT = 15; //15;
+    private static final int MAX_EXP_SORT = 21; //21;
+    private static final int MIN_EXP_INS_MEL = 0; //0
+    private static final int MAX_EXP_INS_MEL = 15; //15
+    private static final int MAX_EXP = 20; //20
+
     static public void test0(PriorityQueue c, DataManager dm, boolean debug) {
-        //System.out.println(dm.toString());
-        Vector<Integer> vec0 = dm.getSuffleData(),
-                vec1= dm.getSuffleData(),
-                vec2 = dm.getSuffleData();
-        System.out.println(vec0.toString());
-        for(Object num: vec0){
-            c.insertar((int) num, (int) num);
+        System.out.println("Original array: " + dm.toString());
+        Vector<Integer> vec;
+        for(int i = 0; i < 3; i++) {
+            vec = dm.getSuffleData();
+            System.out.println("Shuffled array: " + vec.toString());
+            for(int num: vec){
+                c.insertar(num, num);
+            }
+            System.out.print("Final array: [ ");
+            while(!c.isEmpty()){
+                System.out.print(c.extraer_siguiente().getValue() + " ");
+            }
+            System.out.println("]");
         }
-        while(!c.isEmpty()){
-            System.out.println(c.extraer_siguiente().getValue());
+    }
+
+    static public void sort_test(PriorityQueue cp, boolean debug, String name) throws IOException {
+        String btree_filename = "/T2/results/sort/" + name + "-sort-" + System.currentTimeMillis() + ".tsv";
+        String fileName = System.getProperty("user.dir") + btree_filename;
+
+        File fd = new File(fileName);
+        fd.getParentFile().mkdir();
+        fd.createNewFile();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fd));
+
+        String header = "i\t2^i\theapify_time\textract_time" + System.lineSeparator();
+
+        StringBuilder sb = new StringBuilder(header);
+        for(int j = MIN_EXP_SORT; j <= MAX_EXP_SORT; j++) {
+            sb.append(j); sb.append("\t");
+            int n = (int) Math.pow(2, j);
+            sb.append(n); sb.append("\t");
+            System.out.println("Creando arreglo tamaño " + n + "...");
+            DataManager dm = new DataManager(n, 0);
+
+            System.out.println("Revolviendo arreglo...");
+            List<Integer> list = dm.getSuffleData(), sorted_list;
+            long start, end;
+            Sort sort = new Sort(list, cp);
+
+            System.out.println("Heapifiando arreglo...");
+            start = System.currentTimeMillis();
+            sort.heapify();
+            end = System.currentTimeMillis();
+            sb.append(end - start); sb.append("\t");
+
+            System.out.println("Extrayendo elementos en orden...");
+            start = System.currentTimeMillis();
+            sorted_list = sort.sort();
+            end = System.currentTimeMillis();
+            sb.append(end - start); sb.append(System.lineSeparator());
         }
-        System.out.println(vec1.toString());
-        for(Object num: vec1){
-            c.insertar((int) num, (int) num);
+
+        writer.write(sb.toString());
+        System.out.println("Escribiendo resultados en " +  fileName + "...");
+        System.out.println("");
+        System.out.println("############################## REPORT (" + name + "-sort) ###########################################################");
+        System.out.println(sb.toString());
+        writer.close();
+    }
+
+    static public void insert_and_melding_test(PriorityQueue cp, boolean debug, String name) throws IOException {
+        String btree_filename = "/T2/results/ins_meld/" + name + "-ins_meld-" + System.currentTimeMillis() + ".tsv";
+        String fileName = System.getProperty("user.dir") + btree_filename;
+
+        File fd = new File(fileName);
+        fd.getParentFile().mkdir();
+        fd.createNewFile();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fd));
+
+        String header = "i\tn=2^" + MAX_EXP + "\tk=2^i\tinsert_time\tmelding_time" + System.lineSeparator();
+        StringBuilder sb = new StringBuilder(header);
+
+        int n = (int) Math.pow(2, MAX_EXP);
+        System.out.println("Creando arreglo tamaño " + n + "...");
+        DataManager dm = new DataManager(n, 0);
+
+        System.out.println("Revolviendo arreglo...");
+        List<Integer> list = dm.getSuffleData(), sorted_list;
+
+        for(int i = MAX_EXP_INS_MEL; i >= MIN_EXP_INS_MEL; i--) {
+            sb.append(i); sb.append("\t");
+            sb.append(n); sb.append("\t");
+            int k = (int) Math.pow(2, i);
+            sb.append(k); sb.append("\t");
+
+            long start, end;
+            InsertAndMelding insMel = new InsertAndMelding(i, list);
+
+            System.out.println("Insertando subarreglos de tamaño " + k + " en subcolas...");
+            start = System.currentTimeMillis();
+            insMel.insercion(cp);
+            end = System.currentTimeMillis();
+            sb.append(end - start); sb.append("\t");
+
+            System.out.println("Aplicando melding en colas...");
+            start = System.currentTimeMillis();
+            PriorityQueue cola_final = insMel.melding();
+            end = System.currentTimeMillis();
+            sb.append(end - start); sb.append(System.lineSeparator());
         }
-        while(!c.isEmpty()){
-            System.out.println(c.extraer_siguiente().getValue());
-        }
-        System.out.println(vec2.toString());
-        for(Object num: vec2){
-            c.insertar((int) num, (int) num);
-        }
-        while(!c.isEmpty()){
-            System.out.println(c.extraer_siguiente().getValue());
-        }
+
+        writer.write(sb.toString());
+        System.out.println("Escribiendo resultados en " +  fileName + "...");
+        System.out.println("");
+        System.out.println("############################## REPORT (" + name + "-ins_meld) ###########################################################");
+        System.out.println(sb.toString());
+        writer.close();
     }
 }
