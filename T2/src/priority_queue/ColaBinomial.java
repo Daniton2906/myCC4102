636a539df6b03e2c0d4209955d4a89882ec219be
Daniton2906 <ddiomedi2906@gmail.com>
2018-05-25 13:01:39 -0400

    Refactoring ColaBinomial

--- T2/src/priority_queue/ColaBinomial.java
+++ T2/src/priority_queue/ColaBinomial.java
@@ -8,18 +8,21 @@ import java.util.List;
 
 public class ColaBinomial extends AbstractQueue {
 
-    private LinkedList<Binomial_Tree> binomial_forest;
+    private myLinkedList binomial_forest;
     private int max_index;
+    private int node_counter;
 
     public ColaBinomial() {
-        this.binomial_forest = new LinkedList<>();
+        this.binomial_forest = new myLinkedList();
         this.max_index = -1;
+        this.node_counter = 0;
     }
 
     private ColaBinomial(Node node) {
-        this.binomial_forest = new LinkedList<>();
-        this.binomial_forest.add(new Binomial_Tree(node));
+        this.binomial_forest = new myLinkedList();
+        this.binomial_forest.addFirst(new BinomialTree(node));
         this.max_index = 0;
+        this.node_counter = 1;
     }
 
     @Override
@@ -28,15 +31,17 @@ public class ColaBinomial extends AbstractQueue {
                 nueva_cola = suma(this.binomial_forest, cola0.binomial_forest);
         this.binomial_forest = nueva_cola.binomial_forest;
         this.max_index = nueva_cola.max_index;
+        this.node_counter++;
     }
 
     @Override
     public Node extraer_siguiente(){
-        Binomial_Tree max_tree = this.binomial_forest.remove(this.max_index);
-        Node max_node = max_tree.node;
-        ColaBinomial nueva_cola = suma(this.binomial_forest, max_tree.binomial_trees);
+        BinomialTree max_tree = this.binomial_forest.pollMax();
+        Node max_node = max_tree.getNode();
+        ColaBinomial nueva_cola = suma(this.binomial_forest, max_tree.getBinTrees());
         this.binomial_forest = nueva_cola.binomial_forest;
         this.max_index = nueva_cola.max_index;
+        this.node_counter = nueva_cola.node_counter;
         return max_node;
     }
 
@@ -62,91 +67,37 @@ public class ColaBinomial extends AbstractQueue {
         return cp;
     }
 
-    private ColaBinomial suma(LinkedList<Binomial_Tree> bfX, LinkedList<Binomial_Tree> bfY) {
+    private ColaBinomial suma(myLinkedList bfX, myLinkedList bfY) {
         ColaBinomial new_cola = new ColaBinomial();
-        int countX = count_nodes(bfX), countY = count_nodes(bfY),
-                iX = 0, iY = 0, k = 0,
-                max_value = -1, max_index = -1;
-        LinkedList<Binomial_Tree> T = new LinkedList<>(), cS = new LinkedList<>();
+        int k = 0, countX = bfX.getNumberOfNodes(), countY = bfY.getNumberOfNodes();
+        myLinkedList T = new myLinkedList(), cS = new myLinkedList();
+        BinomialTree bltreeX = bfX.pollFirst(), bltreeY = bfY.pollFirst();
+
         while(countX > 0 || countY > 0 || T.size() > 0) {
-            boolean xHasBk = countX > 0 && bfX.get(iX).order == k,
-                    yHasBk = countY > 0 && bfY.get(iY).order == k;
+            boolean xHasBk = countX > 0 && bltreeX.getOrder() == k,
+                    yHasBk = countY > 0 && bltreeY.getOrder() == k;
             if (xHasBk) {
-                T.add(bfX.get(iX));
+                T.addLast(bltreeX);
                 countX -= Math.pow(2, k);
-                iX++;
+                bltreeX = bfX.pollFirst();
             } if (yHasBk) {
-                T.add(bfY.get(iY));
+                T.addLast(bltreeY);
                 countY -= Math.pow(2, k);
-                iY++;
+                bltreeY = bfY.pollFirst();
             } k++;
             if (T.size() == 1) {
-                Binomial_Tree new_bltree = T.remove();
-                cS.add(new_bltree);
-                int value = new_bltree.node.getPriority();
-                if(max_index == -1 || value >= max_value) {
-                    max_value = value;
-                    max_index = cS.size() - 1;
-                }
+                BinomialTree new_bltree = T.pollFirst();
+                cS.addLast(new_bltree);
             } else if (T.size() == 2) {
-                T.add(new Binomial_Tree(T.remove(), T.remove()));
+                T.addLast(new BinomialTree(T.pollFirst(), T.pollFirst()));
             } else if (T.size() == 3) {
-                Binomial_Tree new_bltree = T.remove();
-                cS.add(new_bltree);
-                int value = new_bltree.node.getPriority();
-                if(max_index == -1 || value >= max_value) {
-                    max_value = value;
-                    max_index = cS.size() - 1;
-                }
-                T.add(new Binomial_Tree(T.remove(), T.remove()));
+                BinomialTree new_bltree = T.pollFirst();
+                cS.addLast(new_bltree);
+                T.addLast(new BinomialTree(T.pollFirst(), T.pollFirst()));
             }
         }
-        new_cola.max_index = max_index;
         new_cola.binomial_forest = cS;
         return new_cola;
-    }
-
-    private int count_nodes(LinkedList<Binomial_Tree> bl_forest){
-        int res = 0;
-        for (Binomial_Tree bt: bl_forest) {
-            res += (int) Math.pow(2, bt.order);
-        }
-        return res;
-    }
-
-    private class Binomial_Tree {
-
-        private LinkedList<Binomial_Tree> binomial_trees;
-        private int order;
-        private Node node;
-
-
-        private Binomial_Tree(Node node) {
-            this.binomial_trees = new LinkedList<>();
-            this.order = 0;
-            this.node = node;
-        }
-
-        private Binomial_Tree(Binomial_Tree t0, Binomial_Tree t1) {
-            assert t0.order == t1.order;
-            this.binomial_trees = new LinkedList<>();
-            if(t0.node.getPriority() >= t1.node.getPriority()) {
-                this.binomial_trees.addAll(t0.binomial_trees);
-                this.binomial_trees.addLast(t1);
-                this.node = t0.node;
-                this.order = t0.order + 1;
-            } else {
-                this.binomial_trees.addAll(t1.binomial_trees);
-                this.binomial_trees.addLast(t0);
-                this.node = t1.node;
-                this.order = t1.order + 1;
-            }
-        }
-
-        @Override
-        public String toString() {
-            return "B[" + this.order + "] root " + this.node + ":" + this.binomial_trees;
-        }
     }
 
 }
