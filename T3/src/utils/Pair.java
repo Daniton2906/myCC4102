941d31d88aa7ec0bbb88042eeb19878397ca805e
Rukua95 <tswysen95@gmail.com>
2018-08-13 04:25:27 -0300

    Adding Pair class

---
+++ T3/src/utils/Pair.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class Pair {
+    private int a, b;
+
+    public Pair(int _a, int _b) {
+        a = _a;
+        b = _b;
+    }
+
+    public int getFirst() {
+        return a;
+    }
+
+    public int getSecond() {
+        return b;
+    }
+}
